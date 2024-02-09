4277cc30d300e3a56d63707513649d5e9dfb0c47
Valentina Arciniegas <valentina.arciniegas.s@uniautonoma.edu.co>
2024-02-09 11:57:12 -0500

    complete user model

--- src/main/kotlin/com/meneses/workAppService/repository/user/User.kt
+++ src/main/kotlin/com/meneses/workAppService/repository/user/User.kt
@@ -6,5 +6,11 @@ data class User(
     val id: String? = null,
     val username: String,
     val password: String,
-    val city: City? = null
+    val city: City? = null,
+    val firstname: String? = null,
+    val lastname: String? = null,
+    val email: String? = null,
+    val phone: String? = null,
+    val rating: String? = null,
+    val address: String? = null
 )
